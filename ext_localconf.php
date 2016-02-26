2dbe2a45d1eaf5bb6d5038331a4f5e1a42af1150
Frank Berger <fberger@b-factor.de>
2016-02-26 15:56:39 +0100

    Debug problem von packagist paketen die in extentions benötigt werden.

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,3 +2,8 @@
 
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'Classes/Tools/Plugins.php');
+
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Package\\DependencyResolver'] = array(
+    'className' => 'SUDHAUS7\\Sudhaus7Base\\Typo3fixes\\Composerrequire'
+);
