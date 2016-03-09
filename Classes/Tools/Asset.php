19650088b5b4714a144bbc3e2acaa5b0f02f2441
markus <mhofmann@b-factor.de>
2016-03-09 16:59:33 +0100

    Anpassung: Assets auch in Footer hängen

--- Classes/Tools/Asset.php
+++ Classes/Tools/Asset.php
@@ -12,14 +12,19 @@ class Asset {
     /**
      * @param string $ext The Extensionkey
      * @param array $list A list with filenames relative to Resources/Public of $ext
+     * @param bool $footer
      */
-    public static function add($ext, array $list) {
-        if (!isset($GLOBALS['TSFE']->additionalHeaderData[$ext])) $GLOBALS['TSFE']->additionalHeaderData[$ext] = '';
+    public static function add($ext, array $list, $footer = false) {
+        $var = "additionalHeaderData";
+        if ($footer) {
+            $var = "additionalFooterData";
+        }
+        if (!isset($GLOBALS['TSFE']->$var[$ext])) $GLOBALS['TSFE']->$var[$ext] = '';
         foreach ($list as $e) {
             $a = explode('.', $e);
             $suffix = array_pop($a);
             if (method_exists(__CLASS__, $suffix)) {
-                self::$suffix($ext, $e);
+                self::$suffix($ext, $e,$footer);
             }
         }
     }
@@ -27,16 +32,21 @@ class Asset {
     /**
      * @param string $ext The Extensionkey
      * @param string $file A filename relative to Resources/Public of $ext or an URL
+     * @param bool $footer
      */
-    public static function js($ext, $file) {
-        if (!isset($GLOBALS['TSFE']->additionalHeaderData[$ext])) $GLOBALS['TSFE']->additionalHeaderData[$ext] = '';
+    public static function js($ext, $file, $footer = false) {
+        $var = "additionalHeaderData";
+        if ($footer) {
+            $var = "additionalFooterData";
+        }
+        if (!isset($GLOBALS['TSFE']->$var[$ext])) $GLOBALS['TSFE']->$var[$ext] = '';
         if (is_file(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($ext) . 'Resources/Public/' . $file)) {
             $mtime = filemtime(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($ext) . 'Resources/Public/' . $file);
-            $GLOBALS['TSFE']->additionalHeaderData[$ext] .= '
+            $GLOBALS['TSFE']->$var[$ext] .= '
 <script type="text/javascript" src="typo3conf/ext/' . $ext . '/Resources/Public/' . $file . '?' . $mtime . '"></script>
 ';
         } else if (substr($file, 0, 4) == 'http' || substr($file, 0, 2) == '//') {
-            $GLOBALS['TSFE']->additionalHeaderData[$ext] .= '
+            $GLOBALS['TSFE']->$var[$ext] .= '
 <script type="text/javascript" src="' . $file . '"></script>
 ';
         }
@@ -45,16 +55,21 @@ class Asset {
     /**
      * @param string $ext The Extensionkey
      * @param string $file A filename relative to Resources/Public of $ext or an URL
+     * @param bool $footer
      */
-    public static function css($ext, $file) {
-        if (!isset($GLOBALS['TSFE']->additionalHeaderData[$ext])) $GLOBALS['TSFE']->additionalHeaderData[$ext] = '';
+    public static function css($ext, $file,$footer = false) {
+        $var = "additionalHeaderData";
+        if ($footer) {
+            $var = "additionalFooterData";
+        }
+        if (!isset($GLOBALS['TSFE']->$var[$ext])) $GLOBALS['TSFE']->$var[$ext] = '';
         if (is_file(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($ext) . 'Resources/Public/' . $file)) {
             $mtime = filemtime(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($ext) . 'Resources/Public/' . $file);
-            $GLOBALS['TSFE']->additionalHeaderData[$ext] .= '
+            $GLOBALS['TSFE']->$var[$ext] .= '
 <link rel="stylesheet" type="text/css" href="typo3conf/ext/' . $ext . '/Resources/Public/' . $file . '?' . $mtime . '" media="screen"/>
 ';
         } else if (substr($file, 0, 4) == 'http' || substr($file, 0, 2) == '//') {
-            $GLOBALS['TSFE']->additionalHeaderData[$ext] .= '
+            $GLOBALS['TSFE']->$var[$ext] .= '
 <link rel="stylesheet" type="text/css" href="' . $file . '" media="screen"/>
 ';
         }
