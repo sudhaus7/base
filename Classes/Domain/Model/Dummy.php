b6060caf99f203df2700888c7afcaf2ee91aaba2
Frank Berger <fberger@b-factor.de>
2017-07-14 18:50:01 +0200

    3200 Onlineplaner (Veranstaltungen): Ausgabe-Optimierung - Buttons in Kalender

--- Classes/Domain/Model/Dummy.php
+++ Classes/Domain/Model/Dummy.php
@@ -11,21 +11,39 @@ namespace SUDHAUS7\Sudhaus7Base\Domain\Model;
 
 class Dummy
 {
-    private $data = array();
-    public function __call($method,$arguments) {
-        $call = substr($method,0,3);
-        $var = strtolower(substr($method,3));
-        if ($call=='get') {
-            if (isset($this->data[$var])) return $this->data[$var];
-            return null;
-        }
-        if ($call == 'set') {
-            $this->data[$var] = $arguments[0];
-        }
-        return false;
-    }
-    public function __construct($data = array())
-    {
-        $this->data = $data;
-    }
+	protected $data = [];
+	public function __construct($data) {
+		$this->data = $data;
+	}
+
+	public function __get($field) {
+		if (isset($this->data[$field])) return $this->data[$field];
+		return null;
+	}
+	public function __set($field,$value) {
+		$this->data[$field]=$value;
+		return $this;
+	}
+	public function __call( $name, $arguments ) {
+		$direction = \substr($name,0,3);
+		$var = self::from_camel_case(\substr($name,3));
+
+		if ($direction == 'set') {
+			$this->data[$var]=$arguments;
+		}
+		if ($direction == 'get') {
+			if (isset($this->data[$var])) {
+				return $this->data[$var];
+			}
+			return null;
+		}
+		return $this;
+	}
+
+	private static function from_camel_case($input) {
+		return ltrim(strtolower(preg_replace('/[A-Z]([A-Z](?![a-z]))*/', '_$0', $input)), '_');
+	}
+	private static function to_camel_case($input) {
+		return preg_replace('/(^|_)([a-z])/e', 'strtoupper("\\2")', $input);
+	}
 }
