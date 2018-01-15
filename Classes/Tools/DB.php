a9ecdca38b327e2fd349c7667c4214d337804ae9
Frank Berger <fberger@b-factor.de>
2018-01-15 17:05:41 +0100

    Neues Feature:
     Generiert das SQL das ausgeführt wird aus einem QueryInterface Query z.b. in Repository Klassen
    
     $query = $this->createQuery();
     \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump(\SUDHAUS7\Sudhaus7Base\Tools\DB::getSqlFromQuery($query));

--- Classes/Tools/DB.php
+++ Classes/Tools/DB.php
@@ -7,4 +7,25 @@ class DB {
     public static function get() {
         return $GLOBALS['TYPO3_DB'];
     }
+
+	/**
+	 * Generiert das SQL das ausgeführt wird aus einem QueryInterface Query z.b. in Repository Klassen
+	 *
+	 * Usage:
+	 * $query = $this->createQuery();
+	 * \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump(\SUDHAUS7\Sudhaus7Base\Tools\DB::getSqlFromQuery($query));
+	 *
+	 * @param \TYPO3\CMS\Extbase\Persistence\QueryInterface $query
+	 *
+	 * @return array
+	 */
+	public static function getSqlFromQuery(\TYPO3\CMS\Extbase\Persistence\QueryInterface $query) {
+		$objectManager = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
+		/** @var \TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser $queryParser */
+		$queryParser = $objectManager->get( \TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser::class);
+		$sql = $queryParser->convertQueryToDoctrineQueryBuilder($query)->getSQL();
+		$params = $queryParser->convertQueryToDoctrineQueryBuilder($query)->getParameters();
+		return ['sql'=>$sql,'params'=>$params];
+	}
+
 }
