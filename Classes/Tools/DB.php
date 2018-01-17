dcf07b408fa16490500e0698a175ecaf225f91cc
Frank Berger <fberger@b-factor.de>
2018-01-17 10:41:13 +0100

    Check Typo3 Version, alternativer weg für < 8.0.0

--- Classes/Tools/DB.php
+++ Classes/Tools/DB.php
@@ -1,6 +1,7 @@
 <?php
 namespace SUDHAUS7\Sudhaus7Base\Tools;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 
 class DB {
     /**
@@ -21,12 +22,30 @@ class DB {
 	 *
 	 * @return array
 	 */
-	public static function getSqlFromQuery(\TYPO3\CMS\Extbase\Persistence\QueryInterface $query) {
-		$objectManager = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
-		/** @var \TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser $queryParser */
-		$queryParser = $objectManager->get( \TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser::class);
-		$sql = $queryParser->convertQueryToDoctrineQueryBuilder($query)->getSQL();
-		$params = $queryParser->convertQueryToDoctrineQueryBuilder($query)->getParameters();
+	public static function getSqlFromQuery(\TYPO3\CMS\Extbase\Persistence\QueryInterface $query,$explainOutput = false) {
+
+		if (VersionNumberUtility::convertVersionNumberToInteger( TYPO3_version) < 8000000) {
+			$params = [];
+			$GLOBALS['TYPO3_DB']->debugOuput = 2;
+			if ( $explainOutput ) {
+				$GLOBALS['TYPO3_DB']->explainOutput = true;
+			}
+			$GLOBALS['TYPO3_DB']->store_lastBuiltQuery = true;
+			$query->toArray();
+			$sql = $GLOBALS['TYPO3_DB']->debug_lastBuiltQuery;
+
+			$GLOBALS['TYPO3_DB']->store_lastBuiltQuery = false;
+			$GLOBALS['TYPO3_DB']->explainOutput        = false;
+			$GLOBALS['TYPO3_DB']->debugOuput           = false;
+
+		} else {
+			$objectManager = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
+			/** @var \TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser $queryParser */
+			$queryParser = $objectManager->get( \TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser::class);
+			$sql = $queryParser->convertQueryToDoctrineQueryBuilder($query)->getSQL();
+			$params = $queryParser->convertQueryToDoctrineQueryBuilder($query)->getParameters();
+		}
+
 		return ['sql'=>$sql,'params'=>$params];
 	}
 
